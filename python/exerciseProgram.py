73884c0752a0009dabf5db7255c67072a8152fd0
voidnologo <void.no.logo@icloud.com>
2014-07-03 12:04:36 -0500

    Updated project file.  Pulled timer class out of tests.py.  Created Exercise Program.

--- python/exerciseProgram.py
+++ python/exerciseProgram.py
@@ -13,17 +13,18 @@ num_of_sets = 2
 
 
 while (timer.set_number <= num_of_sets):
-   print ("BREAK! Up Next:")
+   print ("\nBREAK! Up Next:")
    current_exercises = timer.get_current_exercises()
    print ("Person 1: " + current_exercises[0])
    print ("Person 2: " + current_exercises[1])
    timer.wait(timer.break_time)
-   print ("Person 1: " + current_exercises[0])
-   print ("Person 2: " + current_exercises[1])
+   print ("\n**START")
+   print ("\tPerson 1: " + current_exercises[0])
+   print ("\tPerson 2: " + current_exercises[1])
    timer.wait(timer.time_limit)
    if timer.end_of_set():
       # print ("IN END OF SET: " + str(timer.set_number) + "  " + str(timer.current_cycle))
-      print ("\n\n*** END OF SET : " + str(timer.set_number) + "\n\n")
+      print ("\n\n*** END OF SET : " + str(timer.set_number) + " ***\n\n")
       timer.reset()
    # else:
       # print ("NOT END OF SET: "  + str(timer.set_number) + "  " + str(timer.current_cycle))
